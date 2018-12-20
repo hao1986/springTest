7856764ba0285316f70460ba2c77c29b51122cf7
hao1986 <138515282382139.com>
2018-12-20 17:30:24 +0800

    update

---
+++ src/main/java/com/ziyun/springboot/BaseController.java
@@ -0,0 +1,27 @@
+package com.ziyun.springboot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.util.ClassUtils;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+
+public abstract class BaseController {
+
+	private static final String RESP_CODE = "resp_code";
+
+	private static final String RESP_MSG = "resp_msg";
+
+	protected @ResponseBody String responseToJson(int code, String msg, Object... object) {
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put(RESP_CODE, code);
+		resultMap.put(RESP_MSG, msg);
+		for (Object obj : object) {
+			resultMap.put(ClassUtils.getShortName(obj.getClass()).toLowerCase(), obj);
+		}
+		return JSON.toJSONString(resultMap);
+	}
+
+}
--- src/main/java/com/ziyun/springboot/SpringDemoApplication.java
+++ src/main/java/com/ziyun/springboot/SpringDemoApplication.java
@@ -1,4 +1,4 @@
-package com.ziyun.springDemo;
+package com.ziyun.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
